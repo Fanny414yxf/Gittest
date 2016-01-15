65ae7f71483572a24684f483a9c1c270f568af80
Fanny414yxf <634178298@qq.com>
2016-01-15 14:04:20 +0800

    Initial Commit

---
+++ Git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git
+//
+//  Created by 杨晓芬 on 16/1/15.
+//  Copyright © 2016年 Fanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git
+//
+//  Created by 杨晓芬 on 16/1/15.
+//  Copyright © 2016年 Fanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
