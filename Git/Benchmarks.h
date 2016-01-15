7eb8e53fcf584a81d0eb926d1dae1b782b7d1ee8
Fanny414yxf <634178298@qq.com>
2016-01-15 14:48:31 +0800

    add add benchmarksb

---
+++ Git/Benchmarks.h
@@ -0,0 +1,13 @@
+//
+//  Benchmarks.h
+//  Git
+//
+//  Created by 杨晓芬 on 16/1/15.
+//  Copyright © 2016年 Fanny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Benchmarks : NSObject
+
+@end
---
+++ Git/Benchmarks.m
@@ -0,0 +1,13 @@
+//
+//  Benchmarks.m
+//  Git
+//
+//  Created by 杨晓芬 on 16/1/15.
+//  Copyright © 2016年 Fanny. All rights reserved.
+//
+
+#import "Benchmarks.h"
+
+@implementation Benchmarks
+
+@end
